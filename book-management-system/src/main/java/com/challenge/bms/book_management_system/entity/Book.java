969cd5cd577ef237ce6777762c7b321b25d3cf1b
Hugo Januario <128010237+hugaojanuario@users.noreply.github.com>
2025-11-29 19:43:04 -0300

    Entidade do Banco criada com validacoes

---
+++ book-management-system/src/main/java/com/challenge/bms/book_management_system/entity/Book.java
@@ -0,0 +1,45 @@
+package com.challenge.bms.book_management_system.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    @Column
+    @NotBlank
+    private String title;
+
+    @Column
+    @NotBlank
+    private String author;
+
+    @Column
+    @NotBlank
+    private String isbn;
+
+    @Column
+    @NotBlank
+    @Min(value = 1500, message = "Somente sao aceitos livros a partir de 1500")
+    @Max(value = 2025, message = "Somente sao aceitos livros até 2025")
+    private int publicationYear;
+
+
+}
---
+++ book-management-system/src/main/java/com/challenge/bms/book_management_system/repository/BookRepository.java
@@ -0,0 +1,4 @@
+package com.challenge.bms.book_management_system.repository;
+
+public class BookRepository {
+}
