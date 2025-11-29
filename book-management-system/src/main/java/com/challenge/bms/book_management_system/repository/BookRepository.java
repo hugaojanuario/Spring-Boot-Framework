3e3f783f5e5129d5f498e9c6a6f0c12a77bb1402
Hugo Januario <128010237+hugaojanuario@users.noreply.github.com>
2025-11-29 19:46:56 -0300

    Repository configurado com JPA passando um Long como chave

--- book-management-system/src/main/java/com/challenge/bms/book_management_system/repository/BookRepository.java
+++ book-management-system/src/main/java/com/challenge/bms/book_management_system/repository/BookRepository.java
@@ -1,4 +1,7 @@
 package com.challenge.bms.book_management_system.repository;
 
-public class BookRepository {
+import com.challenge.bms.book_management_system.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
